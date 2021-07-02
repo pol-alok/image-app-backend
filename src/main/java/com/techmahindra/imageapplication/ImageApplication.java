dd20f735876ff9364b0362b54919464167411ab7
Ravikant Pal <apps.ravikant@gmail.com>
2021-07-02 08:10:45 +0530

    fixed cors problem

--- src/main/java/com/techmahindra/imageapplication/ImageApplication.java
+++ src/main/java/com/techmahindra/imageapplication/ImageApplication.java
@@ -2,6 +2,9 @@ package com.techmahindra.imageapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -12,4 +15,13 @@ public class ImageApplication {
 		SpringApplication.run(ImageApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000","https://pol-alok.github.io/image-app");
+			}
+		};
+	}
 }
