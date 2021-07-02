bbf653e27fe89d9cbf5cd144e7e31ca1377c5402
Ravikant Pal <apps.ravikant@gmail.com>
2021-07-02 10:47:13 +0530

    fixed cors problem for github

--- src/main/java/com/techmahindra/imageapplication/ImageApplication.java
+++ src/main/java/com/techmahindra/imageapplication/ImageApplication.java
@@ -20,7 +20,7 @@ public class ImageApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000","https://pol-alok.github.io/image-app");
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000","https://pol-alok.github.io");
 			}
 		};
 	}
